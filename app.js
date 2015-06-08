b664b2c72ab5b840d195e1070ac10931c81b972a
Tiffany Chow <tiffachow@gmail.com>
2015-06-08 18:21:01 -0400

    added cors config to server

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.use(session({
 
 app.use('/', routes);
 
+// CORS config
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://picllery-user-images.s3.amazonaws.com/*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
